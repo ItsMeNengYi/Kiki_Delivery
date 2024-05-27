c92954309ba97fae93ed911c0a705f9ec91ce8ab
Lee Ze Hao <zehaolee2004@gmail.com>
2024-05-27 19:39:54 +0800

    added default react navigation App.js file, works on Android + Web

--- ClientRNApp/App.js
+++ ClientRNApp/App.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-const App = () => {
+function HomeScreen() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'purple',
-      }}>
-      <Text>Silly Placeholder</Text>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
     </View>
   );
-};
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
 export default App;
