7cfaceacc40fb18c0c8286d44878f5b900641933
Ren Yick Gan <20040408gry@gmail.com>
2024-06-23 16:59:43 +0800

    Changed environment variables format

--- Client/WebRTC/webRTC.js
+++ Client/WebRTC/webRTC.js
@@ -4,8 +4,16 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import { isEqual } from 'lodash'
 
-const firebaseConfig = JSON.parse(import.meta.env.VITE_FIREBASE_CONFIG);
-
+const firebaseConfig = {
+    apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
+    databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDERID,
+    appId: import.meta.env.VITE_FIREBASE_APPID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENTID
+};
 const servers = {
   iceServers: [
     {
