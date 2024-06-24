c1925068f18241939ccdc5400deb904b476eef5f
Ren Yick Gan <20040408gry@gmail.com>
2024-06-24 09:52:55 +0800

    simple user authentication on client control page

--- Client/main.js
+++ Client/main.js
@@ -1,8 +1,20 @@
 import "/style.css"
 
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 import WebRTC from "/WebRTC/webRTC";
 import VirtualJoystick from '/components/Joystick/joystick';
 
+const firebaseConfig = {
+    apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
+    databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDERID,
+    appId: import.meta.env.VITE_FIREBASE_APPID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENTID
+};
 
 let _droneControl = {
     message:"none",
@@ -12,13 +24,51 @@ let _droneControl = {
     }  
 }
 
-
 const webrtc = new WebRTC();
 webrtc.initializeMediaStream();
 webrtc.initializeDataStream();
 
 const connectButton = document.getElementById('connectButton');
-connectButton.onclick = () => webrtc.initializeConnection();
+connectButton.textContent = 'Authenticating...';
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+
+const database = firebase.firestore();
+function getQueryParameter(name) {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(name);
+}
+
+const userEmail = getQueryParameter('email');
+const userKey = getQueryParameter('key');
+let userExist = false
+let access = false
+const accessCodeDoc = (await database.collection('access_code').doc('00001').get());
+const accessCode = accessCodeDoc.data().curr_access_code;
+const userSnapshot = (await database.collection('user_data').where("email", "==", userEmail).get());
+
+if (!userSnapshot.empty) {
+    const user = userSnapshot.docs[0].data();
+    userExist = true
+  } else {
+    console.log("No such user!");
+}
+
+if (accessCode === userKey && userExist) {
+    console.log("access granted!")
+    access = true
+    connectButton.style.backgroundColor = 'green';
+    connectButton.textContent = 'Connect to Drone';
+    connectButton.onclick = () => webrtc.initializeConnection();
+} else {
+    console.log("access failed");
+    connectButton.style.backgroundColor = 'red';
+    connectButton.textContent = 'Authentication Failed';
+}
+
+
 
 document.getElementById('send').onclick = async ()=> {
     _droneControl.message = document.getElementById('inputForDrone').value;
@@ -58,14 +108,3 @@ const cameraJoystick = new VirtualJoystick(cameraJoystickContainer, {
     }
 }
 );
-
-function getQueryParameter(name) {
-    const urlParams = new URLSearchParams(window.location.search);
-    return urlParams.get(name);
-}
-
-// Get the 'name' and 'key' query parameters
-const userName = getQueryParameter('name');
-const userKey = getQueryParameter('key');
-
-console.log(userName + userKey)
