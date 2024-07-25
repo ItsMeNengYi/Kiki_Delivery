d11858d431edb7cf38d16d590937e6860468f5be
Lee Ze Hao <zehaolee2004@gmail.com>
2024-07-26 06:35:24 +0800

    PasswordResetSuccessScreen Android + Web UI updated

--- ClientRNApp/screens/PasswordResetSuccessScreen.js
+++ ClientRNApp/screens/PasswordResetSuccessScreen.js
@@ -1,57 +1,138 @@
-import { KeyboardAvoidingView, StyleSheet, Text, TextInput, Touchable, TouchableOpacity, View } from 'react-native'
+import { KeyboardAvoidingView, StyleSheet, Text, TextInput, Touchable, TouchableOpacity, View, Platform, Image, ImageBackground } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { FIREBASE_AUTH } from '../firebaseConfig.js'
 import { useNavigation } from '@react-navigation/native'
 
-const PasswordResetSuccessScreen = () => {
+const PasswordResetSuccessScreenAndroid = () => {
   const auth = FIREBASE_AUTH;
   const navigation = useNavigation();
 
   return (
-    <View
-        style={styles.container}
-        behavior='padding'
-    >
-      <Text style={styles.title}>Password Reset Email Sent</Text>
-      <Text style={styles.statusText}>Follow link in email to reset password</Text>
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity
-          onPress={() => {
-            auth.signOut();
-            navigation.replace('Login');
-          }}
-          style={styles.button}
+    <View style={styles.container}>
+      <ImageBackground source={require('../assets/images/android_login_background.png')} resizeMode='cover' style={styles.backgroundImage}>
+        <View
+            style={styles.keyboardAvoidingContainer}
+            behavior='padding'
         >
-          <Text style={styles.buttonText}>Back to Login</Text>
-        </TouchableOpacity>
-      </View>
+          <View style={styles.titleContainer}>
+            <Text style={styles.title}>Password Reset Email Sent</Text>
+            <Text style={styles.subtitle}>Follow link in email to reset password.</Text>
+          </View>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity
+              onPress={() => {
+                auth.signOut();
+                navigation.replace('Login');
+              }}
+              style={styles.button}
+            >
+              <Text style={styles.buttonText}>Back to Login</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </ImageBackground>
     </View>
   )
 }
 
-export default PasswordResetSuccessScreen 
+const PasswordResetSuccessScreenWeb = () => {
+  const auth = FIREBASE_AUTH;
+  const navigation = useNavigation();
+
+  return (
+    <View style={webStyles.container}>
+      <ImageBackground source={require('../assets/images/web_login_background.jpg')} resizeMode='cover' style={webStyles.backgroundImage}>
+        <View
+            style={webStyles.whiteContainer}
+            behavior='padding'
+        >
+          <View style={webStyles.titleContainer}>
+            <Text style={webStyles.title}>Password Reset Email Sent</Text>
+            <Text style={webStyles.subtitle}>Follow link in email to reset password.</Text>
+          </View>
+          <View style={webStyles.buttonContainer}>
+            <TouchableOpacity
+              onPress={() => {
+                auth.signOut();
+                navigation.replace('Login');
+              }}
+              style={webStyles.button}
+            >
+              <Text style={webStyles.buttonText}>Back to Login</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </ImageBackground>
+    </View>
+  )
+}
+
+var exportedScreen;
+if (Platform.OS === 'android') {
+  exportedScreen = PasswordResetSuccessScreenAndroid 
+} else if (Platform.OS === 'web') {
+  exportedScreen = PasswordResetSuccessScreenWeb 
+} else {
+  exportedScreen = PasswordResetSuccessScreenAndroid 
+}
+
+export default exportedScreen;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  backgroundImage: {
+    flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
+  },
+  keyboardAvoidingContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 35
+  },
+  titleContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 20
   },
   title: {
-    fontSize: 20,
-    fontWeight: '500',
-    paddingBottom: 20
+    textAlign: 'center',
+    marginHorizontal: 20,
+    color: 'red',
+    fontWeight: '680',
+    fontSize: 35,
   },
+  subtitle: {
+    marginTop: 8,
+    fontWeight: '680',
+    fontSize: 17,
+  },
+  inputContainer: { 
+    width: '80%',
+  },
+  input: {
+    backgroundColor: 'white',
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 10,
+    marginTop: 30,
+    borderWidth: 2,
+    borderColor: 'coral',
+    fontSize: 16
+  },
+
   buttonContainer: {  
     width: '60%',
     justifyContent: 'center',
     alignItems: 'center',
-    marginTop: 40
+    marginTop: 20
   },
   button: {
     backgroundColor: 'coral',
     width: '100%',
-    padding: 15,
+    padding: 12,
     borderRadius: 10,
     alignItems: 'center'
   },
@@ -60,12 +141,77 @@ const styles = StyleSheet.create({
     fontWeight: '700',
     fontSize: 16
   },
-  statusText: {
-    width: '80%',
+})
+
+const webStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  backgroundImage: {
+    flex: 1,
     justifyContent: 'center',
-    alignContent: 'center',
-    textAlign: 'center',
-    textAlignVertical: 'center',
-    fontSize: 18
-  }
+  },
+  whiteContainer: {
+    backgroundColor: 'white',
+    height: '110%',
+    width: '50%',
+    alignSelf: 'flex-end',
+    justifyContent: 'center',
+    alignItems: 'left',
+    marginBottom: 35
+  },
+  backgroundImage: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  titleContainer: {
+    justifyContent: 'center',
+    alignItems: 'left',
+    marginLeft: 50,
+    marginBottom: 20
+  },
+  title: {
+    color: 'red',
+    fontWeight: '620',
+    fontSize: 50,
+  },
+  subtitle: {
+    color: 'dark grey',
+    fontWeight: '620',
+    fontSize: 20,
+  },
+  inputContainer: { 
+    width: '60%',
+    marginLeft: 40
+  },
+  input: {
+    backgroundColor: 'white',
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 10,
+    marginTop: 5,
+    borderWidth: 2,
+    borderColor: 'coral',
+    fontSize: 16
+  },
+
+  buttonContainer: {  
+    justifyContent: 'left',
+    alignItems: 'left',
+    marginLeft: 40,
+    marginTop: 15,
+    flexDirection: 'row'
+  },
+  button: {
+    backgroundColor: 'coral',
+    width: 300,
+    padding: 12,
+    borderRadius: 10,
+    alignItems: 'center'
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: '700',
+    fontSize: 16
+  },
 })
